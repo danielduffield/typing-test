92144c7510ce167bcec39935b34ff99288b2013e
Daniel Duffield <danieljduffield@gmail.com>
2017-06-09 15:24:47 -0700

    Added an event listener so that the first letter of the test text is selected upon DOM loading. #3

--- main.js
+++ main.js
@@ -9,20 +9,25 @@ function stringToSpannedLetters(string){
     var $letterSpan
     $letterSpan = document.createElement('span')
     $letterSpan.textContent = string[i]
-    $letterSpan.setAttribute('id', 'letter-' + i)
+    $letterSpan.setAttribute('id', 'char-' + i)
     $spannedLetters.appendChild($letterSpan)
   }
   return $spannedLetters
 }
 
 function selectLetter(index) {
-  var selectedLetter = document.getElementById('letter-' + index)
+  var selectedLetter = document.getElementById('char-' + index)
   selectedLetter.setAttribute('class', 'selected')
+  console.log(selectedLetter)
 }
 
 function main(){
   var $spannedLetters = stringToSpannedLetters($testText.textContent)
   $container.replaceChild($spannedLetters, $testText)
+  document.addEventListener('DOMContentLoaded', function(){
+    console.log('Dom loaded')
+    selectLetter(0)
+  })
 }
 
 main()
