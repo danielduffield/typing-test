502c4efcad391a8d9b51ab143ffbf30982b19505
Daniel Duffield <danieljduffield@gmail.com>
2017-06-09 14:29:02 -0700

    Created a function to turn a string into a series of spans wrapping each letter, contained in a parent span. #3

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 var $testText = getElementById('test-text')
+
+function stringToSpannedLetters(string){
+  var $spannedLetters
+  $spannedLetters = document.createElement('span')
+  for (let i = 0; i < string.length; i++) {
+    var $letterSpan
+    $letterSpan = document.createElement('span')
+    $letterSpan.textContent = string[i]
+    $spannedLetters.appendChild($letterSpan)
+  }
+  return $spannedLetters
+}
