063b4ba5045b233063b50545325434673b13cfe2
Daniel Duffield <danieljduffield@gmail.com>
2017-06-09 14:44:09 -0700

    Added unique id tags to each span in string to spanned letters function. #3

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-var $testText = getElementById('test-text')
+var $testText = document.getElementById('test-text')
 
 function stringToSpannedLetters(string){
   var $spannedLetters
-  $spannedLetters = document.createElement('span')
+  $spannedLetters = document.createElement('div')
   for (let i = 0; i < string.length; i++) {
     var $letterSpan
     $letterSpan = document.createElement('span')
     $letterSpan.textContent = string[i]
+    $letterSpan.setAttribute('id', 'letter-' + i)
     $spannedLetters.appendChild($letterSpan)
   }
   return $spannedLetters
+}
+
+function selectLetter(processedString) {
+
 }
